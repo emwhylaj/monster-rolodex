87eb89882b4bfa6e56316bb1072f84a475d7632f
MuyiwaOlalekan <muyiwa.olalekan@thebulb.africa>
2021-07-22 17:19:37 +0100

    finalising the static app

--- src/App.js
+++ src/App.js
@@ -1,34 +1,43 @@
-import './App.css'
-import React, { Component } from 'react'
-import CardList from './components/Card-list/Card-list.component'
+import "./App.css";
+import React, { Component } from "react";
+import CardList from "./components/Card-list/Card-list.component";
+import SearchBox from "./components/Search-box/search-box.component";
 
 class App extends Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
       monsters: [],
-      searchField: '',
-    }
+      searchField: "",
+    };
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
+    fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) => this.setState({ monsters: users }))
+      .then((users) => this.setState({ monsters: users }));
   }
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonster = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        <input
-          type="search"
-          placeholder="search monster"
-          onChange={e => {this.setState({ searchField: e.target.value })}}/>
-        <CardList monsters={this.state.monsters} />
+        <h1>Monster Rolodex</h1>
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonster} />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
